7070971833e1506e7de5bb346ca1763b2af4fbbb
Benjamin Jimenez <Benjamin-Jimenez@outlook.com>
2025-04-16 18:22:25 -0500

    added new components

--- src/app/features/header/header.component.ts
+++ src/app/features/header/header.component.ts
@@ -2,6 +2,10 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import '@esri/calcite-components/components/calcite-shell';
 import '@esri/calcite-components/components/calcite-navigation';
 import '@esri/calcite-components/components/calcite-navigation-logo';
+import '@esri/calcite-components/components/calcite-button';
+import '@esri/calcite-components/components/calcite-dropdown';
+import '@esri/calcite-components/components/calcite-dropdown-group';
+import '@esri/calcite-components/components/calcite-dropdown-item';
 
 @Component({
   selector: 'app-header',
